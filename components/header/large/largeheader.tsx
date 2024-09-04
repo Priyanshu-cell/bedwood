a22e8a6054b4d90a2f68c8661aaa3a40d82516d0
Anuj9675 <anujupadhyay6265@gmail.com>
2024-09-04 11:57:19 +0530

    decrease the speed of sidebar transition

--- components/header/large/largeheader.tsx
+++ components/header/large/largeheader.tsx
@@ -139,7 +139,7 @@ export const LargeHeader: React.FC<LargeHeaderProps> = ({
         <div
           className={`fixed inset-0 bg-white transform ${
             isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-          } transition-transform duration-300 ease-in-out z-50`}
+          } transition-transform duration-500 ease-in-out z-50`}
           style={{ overflow: 'hidden' }}
         >
           <div className="flex items-center p-4">
